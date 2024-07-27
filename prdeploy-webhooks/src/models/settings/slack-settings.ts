7b548aaffd0e558adc117c66c5b4a5fa46ac06ae
Gregg B. Jensen <greggbjensen@users.noreply.github.com>
2024-07-26 23:25:59 -0600

    Move EMAIL_ALIASES to SLACK_EMAIL_ALIASES

--- prdeploy-webhooks/src/models/settings/slack-settings.ts
+++ prdeploy-webhooks/src/models/settings/slack-settings.ts
@@ -1,8 +1,10 @@
+import { EmailAliases } from '../email-aliases';
 import { SlackWebhooksSettings } from './slack-webhooks-settings';
 
 export interface SlackSettings {
   token: string;
   emailDomain: string;
+  emailAliases: EmailAliases;
   webhooks: SlackWebhooksSettings;
   notificationsEnabled: boolean;
 }
