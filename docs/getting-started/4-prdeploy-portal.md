847934e46bf50765051fb221f5d47108e3f08c75
Gregg B. Jensen <greggbjensen@users.noreply.github.com>
2024-08-15 16:08:55 -0600

    Implement script for setting up local kubernetes

--- docs/getting-started/4-prdeploy-portal.md
+++ docs/getting-started/4-prdeploy-portal.md
@@ -1,24 +1,66 @@
-The web portal for **prdeploy** allows you to view and manage settings and deployments.  These are the installation instructions for Kubernetes.
+The web portal for **prdeploy** allows you to view and manage settings and deployments. These are the installation instructions for Kubernetes.
 
-Each repository can have it's own set of environments and services that **prdeploy** manages.  Here is how to configure those settings.
+Each repository can have it's own set of environments and services that **prdeploy** manages. Here is how to configure those settings.
+
+## AWS configuration
+
+1. Create the following IAM policy as `prdeploy-backend`:
+
+```json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:PutParameter",
+        "ssm:GetParametersByPath",
+        "ssm:GetParameter"
+      ],
+      "Resource": [
+        "arn:aws:ssm:{{AWS_REGION}}:{{AWS_ACCOUNT_ID}}:parameter//prdeploy/*"
+      ]
+    }
+  ]
+}
+```
+
+2. Add the following entries as `SecureString` to AWS Parameter Store:
+
+| Name                                 | Value                                                                   |
+| ------------------------------------ | ----------------------------------------------------------------------- |
+| /prdeploy/APP_ID                     | ID from GitHub App.                                                     |
+| /prdeploy/WEBHOOK_SECRET             | Webhook secret configured for GitHub App.                               |
+| /prdeploy/gh_app_key.pem             | Secret app key downloaded from GitHub App.                              |
+| /prdeploy/GitHubAuth\_\_ClientId     | Client ID for GitHub OAuth App.                                         |
+| /prdeploy/GitHubAuth\_\_ClientSecret | Client Secret for GitHub OAuth App.                                     |
+| /prdeploy/Jwt\_\_Key                 | Generated JWT validation key.                                           |
+| /prdeploy/Jwt\_\_TokenEncryptionKey  | Generated JWT token encryption key.<br>Should not be the same as above. |
+
+`NOTE:` To generate a unique encryption key, you can run the following:
+
+```bash
+node -e "console.log(require('crypto').randomBytes(256).toString('base64'));
+```
 
 ## Helm chart install
 
 1. Install the prerequists in Kubernetes:
-    1. ingress-nginx
-    2. cert-manager with letsencrypt
-    3. External secrets
+
+   1. ingress-nginx
+   2. cert-manager with letsencrypt
+   3. External secrets
 
    _NOTE: For effeciency settings cache only updates every 5 minutes._
 
-3. Labels will be automatically created for each environment in your repository.
-    1. Environment labels should be the badge color you want for the environment icon.
-4. In order for a completed or failed deploy status message to show the version, you must provide a `build-details` artifact to your builds that includes a `build-details.json` file with at least the following:
-    1. The [build-details](/.github/actions/build-details/README.md#build-details-action) action can provide this.
+2. Labels will be automatically created for each environment in your repository.
+   1. Environment labels should be the badge color you want for the environment icon.
+3. In order for a completed or failed deploy status message to show the version, you must provide a `build-details` artifact to your builds that includes a `build-details.json` file with at least the following:
+   1. The [build-details](/.github/actions/build-details/README.md#build-details-action) action can provide this.
 
 ```json
 {
-    "version": "2023.11.30-r7040754105"
+  "version": "2023.11.30-r7040754105"
 }
 ```
 
--- scripts/local.py
+++ scripts/local.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 import argparse
+import shutil
+import re
 from dotenv import load_dotenv
 from termcolor import cprint
 
@@ -8,18 +10,89 @@ parser = argparse.ArgumentParser('local.py')
 parser.add_argument("--skip-aws", help="Skips the AWS configuration.", action="store_true")
 parser.add_argument("--skip-prereq", help="Skips the helm rerequisites.", action="store_true")
 parser.add_argument("--skip-docker", help="Skips the docker build of the images.", action="store_true")
+parser.add_argument("--skip-helm", help="Skips the prdeploy helm install.", action="store_true")
 args = parser.parse_args()
 
 load_dotenv()
 
 if not args.skip_aws:
-  cprint(f'## Configuring AWS profile {os.environ["AWS_DEFAULT_PROFILE"]}.', 'cyan')
+  cprint(f'## Configuring AWS profile {os.environ["AWS_DEFAULT_PROFILE"]}', 'cyan')
   p = subprocess.Popen(f'aws configure --profile {os.environ["AWS_DEFAULT_PROFILE"]}', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   p.stdin.write(os.environ["AWS_ACCESS_KEY_ID"].encode() + b'\n')
   p.stdin.write(os.environ["AWS_SECRET_ACCESS_KEY"].encode() + b'\n')
   p.stdin.write(os.environ["AWS_REGION"].encode() + b'\n')
   p.stdin.write(os.environ["AWS_OUTPUT"].encode() + b'\n')
   p.communicate()
+
+  cprint('\n## Creating prdeploy-backend role\n', 'cyan')
+  op = subprocess.run('aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList | [0]"', encoding='utf-8', stdout=subprocess.PIPE)
+  awsOpenIdProvider=op.stdout.rstrip()
+  print(f'Open ID Provider: {awsOpenIdProvider}')
+  if awsOpenIdProvider == 'null':
+    cprint('Creating AWS Open ID Provider')
+    osp = subprocess.Popen(f'openssl s_client -servername oidc.eks.{os.environ["AWS_REGION"]}.amazonaws.com -showcerts -connect oidc.eks.{os.environ["AWS_REGION"]}.amazonaws.com:443', encoding='utf-8', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    osp.stdin.write('Q\n')
+    out, err = osp.communicate()
+    cert=re.search('(-----BEGIN CERTIFICATE-----\n(?:.*\n)+?-----END CERTIFICATE-----)', out).group(1)
+
+    fp = subprocess.run('openssl x509 -fingerprint -noout', input=cert, encoding='utf-8', stdout=subprocess.PIPE)
+    outThumbprint=fp.stdout.rstrip()
+    thumbprint=re.sub('SHA1 Fingerprint=', '', outThumbprint).replace(':', '').lower()
+
+    subprocess.run(f'aws iam create-open-id-connect-provider --client-id-list sts.amazonaws.com --thumbprint-list {thumbprint} --url https://kubernetes.default.svc.cluster.local', encoding='utf-8')
+    op2 = subprocess.run('aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList | [0]"', encoding='utf-8', stdout=subprocess.PIPE)
+    print(f'Open ID Provider: {awsOpenIdProvider}')
+
+  ap = subprocess.run('aws sts get-caller-identity --query Account --output text', encoding='utf-8', stdout=subprocess.PIPE)
+  awsAccountId=ap.stdout.rstrip()
+  print(f'AWS_ACCOUNT_ID: {awsAccountId}')
+
+  if not os.path.exists('temp'):
+    os.mkdir('temp')
+
+  hasRole = False
+  try:
+      subprocess.check_output('aws iam get-role --role-name prdeploy-backend')
+      hasRole = True
+  except:
+    print('Role does not exist yet, creating it.')
+
+  if not hasRole:
+    shutil.copyfile('assets/aws-iam-assume-role-policy.json', 'temp/aws-iam-assume-role-policy.json')
+    with open('temp/aws-iam-assume-role-policy.json', 'r') as file:
+      filedata = file.read()
+
+    filedata = filedata.replace('{{AWS_REGION}}', os.environ["AWS_REGION"])
+    filedata = filedata.replace('{{AWS_ACCOUNT_ID}}', awsAccountId)
+
+    with open('temp/aws-iam-assume-role-policy.json', 'w') as file:
+      file.write(filedata)
+
+    subprocess.run('aws iam create-role --role-name prdeploy-backend --assume-role-policy-document file://temp/aws-iam-assume-role-policy.json')
+
+  print('\nVerifying prdeploy-backend role policy.')
+  shutil.copyfile('assets/aws-iam-policy.json', 'temp/aws-iam-policy.json')
+
+  with open('temp/aws-iam-policy.json', 'r') as file:
+    filedata = file.read()
+
+  filedata = filedata.replace('{{AWS_REGION}}', os.environ["AWS_REGION"])
+  filedata = filedata.replace('{{AWS_ACCOUNT_ID}}', awsAccountId)
+
+  with open('temp/aws-iam-policy.json', 'w') as file:
+    file.write(filedata)
+
+  hasPolicy = False
+  try:
+      subprocess.run(f'aws iam get-policy --policy-arn arn:aws:iam::{awsAccountId}:policy/prdeploy-backend', encoding='utf-8', stdout=subprocess.PIPE)
+      hasPolicy = True
+  except:
+    print('Policy does not exist yet, creating it.')
+
+  if not hasPolicy:
+    subprocess.run('aws iam create-policy --policy-name prdeploy-backend --policy-document file://temp/aws-iam-policy.json')
+
+  subprocess.run(f'aws iam attach-role-policy --policy-arn arn:aws:iam::{awsAccountId}:policy/prdeploy-backend --role-name prdeploy-backend')
 
   cprint('\n## Configuring AWS Parameter Store required values.', 'cyan')
   prdeployParams = ['APP_ID', 'WEBHOOK_SECRET', 'gh_app_key.pem', 'GitHubAuth__ClientId', 'GitHubAuth__ClientSecret', 'Jwt__Key', 'Jwt__TokenEncryptionKey']
@@ -51,6 +124,23 @@ if not args.skip_prereq:
   print('\n')
   subprocess.run('helm upgrade ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.allowSnippetAnnotations=true --install')
 
-cprint('\n## Installing prdeploy chart.\n', 'cyan')
-os.chdir('../charts')
-subprocess.run('helm upgrade prdeploy ./prdeploy --values ../scripts/local-values.yaml --reset-values --namespace prdeploy --install --create-namespace --wait --atomic')
+if not args.skip_helm:
+  cprint('\n## Installing prdeploy chart.\n', 'cyan')
+  awp = subprocess.run('aws sts get-caller-identity --query Account --output text', encoding='utf-8', stdout=subprocess.PIPE)
+  awsAccountId=awp.stdout.rstrip()
+
+  shutil.copyfile('assets/local-values.yaml', 'temp/local-values.yaml')
+
+  with open('temp/local-values.yaml', 'r') as file:
+    filedata = file.read()
+
+  filedata = filedata.replace('{{AWS_REGION}}', os.environ["AWS_REGION"])
+  filedata = filedata.replace('{{AWS_ACCOUNT_ID}}', awsAccountId)
+
+  with open('temp/local-values.yaml', 'w') as file:
+    file.write(filedata)
+
+  os.chdir('../charts')
+  subprocess.run(f'helm upgrade prdeploy ./prdeploy --values ../scripts/temp/local-values.yaml --reset-values --namespace prdeploy --install --create-namespace --wait --atomic')
+
+cprint('\nLocal configuration complete.', 'green')
