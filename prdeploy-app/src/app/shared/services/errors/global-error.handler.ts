3cba690e35731d00e5bb57409d1ae558e0d2e5f4
Gregg B. Jensen <greggbjensen@users.noreply.github.com>
2024-08-25 19:35:06 -0600

    Add default environment color and handling chunk load error (#79)

--- prdeploy-app/src/app/shared/services/errors/global-error.handler.ts
+++ prdeploy-app/src/app/shared/services/errors/global-error.handler.ts
@@ -6,12 +6,19 @@ import { NotificationManager } from '../../managers';
   providedIn: 'root'
 })
 export class GlobalErrorHandler implements ErrorHandler {
+  private static readonly ChunkFailedMessage = /Loading chunk [\d]+ failed/;
+
   constructor(
     private _loggingService: LoggingService,
     private _notificationManager: NotificationManager
   ) {}
 
   handleError(error: any): void {
+    if (GlobalErrorHandler.ChunkFailedMessage.test(error.message)) {
+      window.location.reload();
+      return;
+    }
+
     this._loggingService.error(error);
     this._notificationManager.show(error.message, 'danger');
   }
